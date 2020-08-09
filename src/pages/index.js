5deb94ef51ae87af4f8f256a0253bcb50afdd2dc
BrightonDube <BrightonDube@users.noreply.github.com>
2020-08-09 15:47:19 +0200

    add index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,29 @@
 import React from "react"
 import Layout from "../components/Layout" // highlight-line
+import { Row, Col } from "react-bootstrap"
+import image from "../images/italian-cuisine-2378729_640.jpg"
 
 export default function Home() {
   return (
     <Layout>
-      {" "}
-      {/* highlight-line */}
-      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-      <p>
-        What do I like to do? Lots of course but definitely enjoy building
-        websites.
-      </p>
+      <Row>
+        <Col className="text-center col-4">
+          <h1>The Home of Fresh Food</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+            ratione repellendus doloribus quo, in commodi voluptatum provident
+            doloremque consequatur possimus sapiente. Veritatis excepturi, error
+            libero ex ad consequatur voluptates dolor?
+          </p>
+        </Col>
+        <Col xs={12} lg={8}>
+          <img src={image} alt="home" className="img-responsive" />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          fugit tempora natus nobis molestias delectus esse beatae obcaecati.
+          Perferendis ad quo facilis illum harum autem vitae exercitationem
+          eaque tempora voluptates.
+        </Col>
+      </Row>
     </Layout>
   )
 }
