b00d858ebc227436177abc74f0f8448208651f69
BrightonDube <BrightonDube@users.noreply.github.com>
2020-08-09 17:54:32 +0200

    add more content

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,24 @@ export default function Home() {
           eaque tempora voluptates.
         </Col>
       </Row>
+      <Row>
+        <Col className="text-center col-4">
+          <h1>The Home of Fresh Food</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+            ratione repellendus doloribus quo, in commodi voluptatum provident
+            doloremque consequatur possimus sapiente. Veritatis excepturi, error
+            libero ex ad consequatur voluptates dolor?
+          </p>
+        </Col>
+        <Col xs={12} lg={8}>
+          <img src={image} alt="home" className="img-responsive" />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          fugit tempora natus nobis molestias delectus esse beatae obcaecati.
+          Perferendis ad quo facilis illum harum autem vitae exercitationem
+          eaque tempora voluptates.
+        </Col>
+      </Row>
     </Layout>
   )
 }
